eb9b9730ce6e03162d980aae7d23800d18551272
Max <mxkltn@gmail.com>
2024-02-23 23:03:30 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://b13177eb-6fd6-4a9f-937d-536e8a246134.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Uchiha",
+    "lastName": "Naruto",
+    "address": "Konoha, 143 apt.",
+    "metroStation": 3,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-20",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
